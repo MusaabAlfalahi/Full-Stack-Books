362a51321588adb63e2accd8f3bbf551d1c9dad3
MusaabAlfalahi <mosaab.dulimi@yahoo.com>
2023-12-03 02:17:55 +0300

    Home & details pages

---
+++ frontend/src/pages/ShowBook.jsx
@@ -0,0 +1,65 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/spinner";
+import moment from 'moment';
+
+const ShowBook = () => {
+  const [book, setBook] = useState({});
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:3000/book/${id}`)
+      .then((res) => {
+        setBook(res.data.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Show Book</h1>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4">
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Id</span>
+            <span>{book.id}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Title</span>
+            <span>{book.title}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Author</span>
+            <span>{book.author}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Publish Year</span>
+            <span>{book.publishYear}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Create Time</span>
+            <span>{moment(book.createdAt).format('MMMM Do YYYY')}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Last Update Time</span>
+            <span>{moment(book.updatedAt).fromNow()}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ShowBook;
